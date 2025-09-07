fc82e43f0591ba8b75ad2d7d8b12af1f2379c9f1
Ejay02 <e.jae02@gmail.com>
2025-09-07 19:24:59 +0100

    feat(auth): integrate better-auth for user authentication and session management

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,58 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { authClient } from "@/lib/auth-client";
 
 export default function Home() {
-  return <Button>Click me</Button>;
+
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = () => {
+    authClient.signUp.email(
+      {
+        email, // user email address
+        password, // user password -> min 8 characters by default
+        name, // user display name
+      },
+      {
+        onRequest: () => {
+          //show loading
+        },
+        onSuccess: () => {
+          //redirect to the dashboard or sign in page
+          window.alert("User created successfully!");
+        },
+        onError: () => {
+          // display the error message
+          window.alert("Something went wrong!");
+        },
+      }
+    );
+  };
+
+  return (
+    <div className="p-4 flex flex-col gap-y-4">
+      <Input
+        placeholder="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <Input
+        placeholder="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <Input
+        placeholder="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <Button onClick={onSubmit}>Create User</Button>
+    </div>
+  );
 }
